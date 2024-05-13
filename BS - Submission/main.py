b3b6152b24e5456e718778afe2c5869cd2e73bce
Raj Dubey <55090463+rajdubey09@users.noreply.github.com>
2024-05-13 11:16:06 +0530

    initial commit

---
+++ BS - Submission/main.py
@@ -0,0 +1,23 @@
+import cv2
+import pixellib
+from pixellib.instance import instance_segmentation
+
+
+segment_image = instance_segmentation()
+segment_image.load_model("mask_rcnn_coco.h5")
+
+camera = cv2.VideoCapture(0)
+
+while camera.isOpened():
+    res, frame = camera.read()
+    ## Applying Segmentation
+    result = segment_image.segmentFrame(frame,show_boxes=True)
+    # image = result[1]
+    image = frame
+    cv2.imshow("Image Segmentation",image)
+
+    if cv2.waitKey(10) & 0xFF==ord('q'):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
